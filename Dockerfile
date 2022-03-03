99f06864e4c38c80653ebf94378f8249cbf3f3ea
hhs <rosehs@naver.com>
2022-03-03 17:20:41 +0900

    update5

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,18 @@
+FROM node:alpine as builder
+
+WORKDIR '/usr/src/app'
+
+COPY package.json .
+COPY package-lock.json ./
+
+RUN yarn
+
+# 로컬에 node_module 있으면 지워줄 것
+COPY ./ ./
+
+RUN yarn build
+
+# run stage
 FROM nginx
-COPY build /usr/share/nginx/html
+EXPOSE 80
+COPY --from=builder /usr/src/app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>hello hyesun--update3</code> and save to reload.
+          Edit <code>hello hyesun--update5</code> and save to reload.
         </p>
         <a
           className="App-link"
